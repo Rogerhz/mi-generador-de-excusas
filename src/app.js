d6a41c49704ecaaeff5c5628e7161f46102578ea
Rogerhz <rogerhernandez0260@gmail.com>
2021-03-19 20:46:23 -0400

    Mi generador de excusas

--- src/app.js
+++ src/app.js
@@ -8,4 +8,35 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  // declarar listas con partes de excusas
+  let who = ["The dog", "My gmarand", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  // escoger de cada lista un elemento aleatoreo
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+  // una vez tengamos todas las partes, pegamos todo en la cadena
+  // let excuse =
+  //   randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+  // let excuse1 = randomWho.concat(
+  //   " ",
+  //   randomAction,
+  //   " ",
+  //   randomWhat,
+  //   " ",
+  //   randomWhen
+  // );
+  let excuses = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`; // !!!mejor metodo!!!!
+  // imprimir la excusa
+  document.querySelector("#excuses").innerHTML = excuses;
 };
